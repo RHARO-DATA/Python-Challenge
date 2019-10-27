219b7fb3e8fdfef0ffaa1feb94744630454b8d84
Raul <56462668+RHARO-DATA@users.noreply.github.com>
2019-10-26 19:20:07 -0500

    Add files via upload

---
+++ Budget_data/Budget_Data.py
@@ -0,0 +1,67 @@
+import os
+import csv
+
+
+# Path to file
+budget_csv = os.path.join("..", "Resources","budget_data.csv")
+
+date = []            # List for the dates
+profit_losses = []   # List for Profit / Losses 
+diferience = []      # List for the differiece between months  
+total = 0            # Variable for Total amount
+month2 = []          # List to copy elements from Profit / Losses
+
+
+
+
+with open(budget_csv,'r', newline= "") as csvfile:
+
+     # Create the csv reader object
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    
+
+    csv_header = next(csvreader)   
+
+    # Read each row of data 
+    for row in csvreader:
+
+        date.append(row[0])             # add dates to the lis
+        profit_losses.append(row[1])    # add profit / losses to the list
+        total = total + int(row[1])     # Total amount
+
+
+
+    month2 = profit_losses
+
+    
+    for i in range (len(date)-1):        # calculate diferience between months
+
+        diferience.append(int(month2[i+1])-int(month2[i]))  # add diferience to the list 
+
+
+   
+    # Number of months
+    num_months = len(date)      
+
+    #calculate the average of Profit / Losses 
+    average = round((sum(diferience)) / (num_months -1),2)
+
+    # Get max increase in profits
+    maxi = max(diferience)
+
+    # Get decrease in losses
+    mini = min(diferience)
+    
+
+    # Print Analysis
+    
+    print(" Financial Analysis ")
+    print("------------------------------------------")
+    print(f"Total Months : {num_months} ")
+    print(f"Total : {total} ")
+    print(f"Average Change : {average}")
+    print(f"Greatest increase in Profits: ${maxi} ")
+    print(f"Greatest decrease in Profits: ${mini} ")
+
+
+
---
+++ Homework PyPoll/PyPoll/PyPoll/PyPoll.py
@@ -0,0 +1,70 @@
+import csv
+import os
+
+# Path to file
+election_csv = os.path.join("..", "Resources","election_data.csv")
+
+votes = []   #List to count the votes
+khan = 0     #Variable to count Khan Votes     
+correy = 0   #Variable to count Correy Votes     
+li = 0       #Variable to count Li Votes     
+toley = 0    #Variable to count O'Tooley Votes    
+
+
+
+with open(election_csv,'r', newline= "") as csvfile:
+    # Create the csv reader object
+    csvreader = csv.reader(csvfile, delimiter = ',')    
+
+
+    for row in csvreader:
+
+        votes.append(row[0])      #add Votes to the List
+
+        if row[2] == "Khan":
+            khan = khan + 1       #count votes for Khan
+
+        elif row[2] == "Correy":
+            correy = correy + 1   #count votes for Correy
+
+        elif row[2] == "Li":
+            li = li + 1           #count votes for Li
+
+        elif row[2] == "O'Tooley":
+            toley = toley + 1      #count votes for O'Tooley
+
+
+# Total Votes
+total_votes = int(len(votes))
+
+
+total_khan =round( (khan * 100) / total_votes)        #Total votes for Kham
+total_correy = round( (correy * 100) / total_votes)   #Total votes for Correy
+total_li =round( (li * 100) / total_votes)            #Total votes for Li
+total_toley =round( (toley * 100) / total_votes)      #Total votes for O'Tooley
+
+
+# Analysis Print
+print("Election Results")
+print("--------------------------------------")
+print(f"Total Votes:  {total_votes}  ")
+print("--------------------------------------")
+print(f"Khan:     {total_khan}%  ({khan}) ")
+print(f"Correy:   {total_correy}%  ({correy}) ")
+print(f"li:       {total_li}%  ({li}) ")
+print(f"O'Tooley: {total_toley}%  ({toley}) ")
+print("--------------------------------------")
+print("Winner: Khan")
+print("--------------------------------------")
+
+
+
+
+
+
+            
+
+
+
+
+
